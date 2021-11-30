d9685bf4c3d3b4c883e2ee5c9f4e4b5cd9b5d29f
rafaelgust <rafael.appdevs@gmail.com>
2021-11-29 23:08:39 -0300

    game and main

--- js/game.js
+++ js/game.js
@@ -1,3 +1,9 @@
 let board = ['','','','','','','','',''],
 playerTime = 0,
 symbols = ['o', 'x'];
+
+function move(position){
+    board[position] = symbols[playerTime];
+
+    playerTime === 0 ? playerTime = 1 : playerTime = 0;
+}
--- js/main.js
+++ js/main.js
@@ -1,14 +1,25 @@
 
+let squares = document.querySelectorAll('.square');
+
 document.addEventListener('DOMContentLoaded', ()=> {
-
-    let squares = document.querySelectorAll('.square');
-
     squares.forEach((square) => {
         square.addEventListener('click', click);
     });
-    
-    function click(e){
-        e.style.background = red;
-    }
-
 });
+
+function click(e){
+    let square = e.target;
+    let position = square.id;
+
+    move(position);
+    update();
+}
+
+function update(){ 
+    squares.forEach((square) => {
+        let position = square.id;
+        let symbol = board[position];
+
+        if(symbol !== ''){square.innerHTML = `<div class="${symbol}"></div>`;}
+    });
+}
